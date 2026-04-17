902359c49c2bf9a74eb9759feffa242171fa194b
hlam3004 <hlam.30042005@gmail.com>
2026-04-17 20:14:06 +0700

    feat: implement file service

---
+++ lib/services/file_service.dart
@@ -0,0 +1,119 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:drawing_app/core/constants.dart';
+import 'package:drawing_app/models/base_shape.dart';
+import 'package:drawing_app/models/circle_shape.dart';
+import 'package:drawing_app/models/ellipse_shape.dart';
+import 'package:drawing_app/models/line_shape.dart';
+import 'package:drawing_app/models/point_shape.dart';
+import 'package:drawing_app/models/rect_shape.dart';
+import 'package:drawing_app/models/square_shape.dart';
+import 'package:flutter/material.dart';
+
+class FileService {
+  Uint8List convertShapesToBinary(List<BaseShape> shapes) {
+    List<Map<String, dynamic>> rawList = shapes
+        .map((s) => s.toRawData())
+        .toList();
+    String jsonString = jsonEncode(rawList);
+    return utf8.encode(jsonString);
+  }
+
+  Future<void> saveFile(List<BaseShape> shapes, String filePath) async {
+    Uint8List binaryData = convertShapesToBinary(shapes);
+    File file = File(filePath);
+    await file.writeAsBytes(binaryData);
+  }
+
+  Future<List<BaseShape>> loadFile(String filePath) async {
+    File file = File(filePath);
+    if (!await file.exists()) {
+      return [];
+    }
+    Uint8List binaryData = await file.readAsBytes();
+    String jsonString = utf8.decode(binaryData);
+    List<dynamic> rawList = jsonDecode(jsonString);
+    return rawList.map((data) => _parseShape(data)).toList();
+  }
+
+  BaseShape _parseShape(Map<String, dynamic> data) {
+    ShapeType type = ShapeType.values[data['type'] as int];
+    Color color = Color(data['color'] as int);
+    double strokeWidth = (data['strokeWidth'] as num).toDouble();
+
+    // TODO: add isFilled to ...Shape constructor
+    bool isFilled = data['isFilled'] as bool? ?? false;
+
+    switch (type) {
+      case ShapeType.point:
+        return PointShape(
+          color: color,
+          strokeWidth: strokeWidth,
+          point: Offset(
+            (data['x'] as num).toDouble(),
+            (data['y'] as num).toDouble(),
+          ),
+        );
+      case ShapeType.line:
+        return LineShape(
+          color: color,
+          strokeWidth: strokeWidth,
+          start: Offset(
+            (data['x1'] as num).toDouble(),
+            (data['y1'] as num).toDouble(),
+          ),
+          end: Offset(
+            (data['x2'] as num).toDouble(),
+            (data['y2'] as num).toDouble(),
+          ),
+        );
+      case ShapeType.circle:
+        return CircleShape(
+          color: color,
+          strokeWidth: strokeWidth,
+          center: Offset(
+            // TODO: edit after wrote CircleShape.toRawData()
+            (data['centerX'] as num).toDouble(),
+            (data['centerY'] as num).toDouble(),
+          ),
+          radius: (data['radius'] as num).toDouble(),
+        );
+      case ShapeType.square:
+        return SquareShape(
+          color: color,
+          strokeWidth: strokeWidth,
+          topLeft: Offset(
+            (data['x'] as num).toDouble(),
+            (data['y'] as num).toDouble(),
+          ),
+          side: (data['side'] as num).toDouble(),
+        );
+      case ShapeType.rect:
+        return RectShape(
+          color: color,
+          strokeWidth: strokeWidth,
+          topLeft: Offset(
+            (data['x1'] as num).toDouble(),
+            (data['y1'] as num).toDouble(),
+          ),
+          bottomRight: Offset(
+            (data['x2'] as num).toDouble(),
+            (data['y2'] as num).toDouble(),
+          ),
+        );
+      case ShapeType.ellipse:
+        return EllipseShape(
+          color: color,
+          strokeWidth: strokeWidth,
+          bounds: Rect.fromLTRB(
+            (data['left'] as num).toDouble(),
+            (data['top'] as num).toDouble(),
+            (data['right'] as num).toDouble(),
+            (data['bottom'] as num).toDouble(),
+          ),
+        );
+    }
+  }
+}
